89a2f8fe61443959c356af54d7b5f7d4967f9b98
vaibav77 <vaibavv5360@gmail.com>
2025-09-25 00:52:11 -0700

    Add files via upload

---
+++ SqureQube.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int n, i;
+	printf("Enter a positive integer n:");
+	scanf("%d",&n);
+	printf("number\tResult\n");
+	printf("------------------------------------------\n");
+	for(i=1;i<=n;i++)
+	{
+		if(i%2==0)
+		{
+			printf("%d\t%d\n",i,i*i);
+		}
+		else
+		{
+			printf("%d\t%\n",i,i*i);
+		}
+	}
+	return 0;
+	}
